325b9cac7960c910efa2bc051a56dbefeb39d829
Vladimir Romanovsky <vladimir_roman@mail.ru>
2018-12-25 15:49:45 +0500

    Add project files.

---
+++ FurnacesInHand/vdp03.cs
@@ -0,0 +1,23 @@
+namespace FurnacesInHand
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("public.vdp03")]
+    public partial class vdp03
+    {
+        public DateTime? dateandtime { get; set; }
+
+        public int? mks { get; set; }
+
+        [StringLength(50)]
+        public string tagname { get; set; }
+
+        public double? val { get; set; }
+
+        public int id { get; set; }
+    }
+}
---
+++ FurnacesInHand/vdp31.cs
@@ -0,0 +1,23 @@
+namespace FurnacesInHand
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("public.vdp31")]
+    public partial class vdp31
+    {
+        public DateTime? dateandtime { get; set; }
+
+        public int? mks { get; set; }
+
+        [StringLength(50)]
+        public string tagname { get; set; }
+
+        public double? val { get; set; }
+
+        public int id { get; set; }
+    }
+}
