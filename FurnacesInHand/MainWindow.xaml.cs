33cc02ba548e5c0d2f78b79fc67cb8397ae2ea67
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-03-11 16:53:21 +0500

    TreeView Control for choosing a furnace is added

--- FurnacesInHand/MainWindow.xaml.cs
+++ FurnacesInHand/MainWindow.xaml.cs
@@ -68,21 +68,24 @@ namespace FurnacesInHand
         {
             using (var context = new FurnacesModelLocal()) //создали контекст взаимодействия с базой данных
             {
-                var pars = context.vdp03.ToArray();
+                //var pars = context.Database.SqlQuery(typeof(vdp03),"Select tagname,val from vdp03 order by id limit 10 offset 0");
+                //select * from vdp04 order by id limit 100 offset 19592897; -- this SQL string should be put into call
+                //select * from vdp06 order by id limit 10 offset 30348545;  -- or this one
                 conn = context.Database.Connection; //извлекли объект для соединения с БД
                 conn.Open(); //открыли соединение
+                var pars = context.vdp08.Where(x => x.tagname == "Arc_U").OrderBy(x => x.id).Skip(1000000).Take(25).ToArray();
                 MessageBox.Show(String.Format("PostgreSQL version is {0}",conn.ServerVersion));
                 MessageBox.Show($"We have {pars.Length} par(s).");
-                             //for (int i = 0; i < 10; i++)
-                             //MessageBox.Show(pars[i].dateandtime.ToString()+ " " +pars[i].id+" "+pars[i].mks+" "+pars[i].tagname);
-                             //context.vdp03.Load();
-                             //inList = context.vdp03.Local;
-                             //Binding b = new Binding();
-                             //b.Source = new string[] { "11", "22", "33", "11", "22", "33" };
-                             //b.Source = new MyData();
-                             //b.Source = pars;
+                //for (int i = 0; i < 10; i++)
+                //MessageBox.Show(pars[i].dateandtime.ToString()+ " " +pars[i].id+" "+pars[i].mks+" "+pars[i].tagname);
+                //context.vdp03.Load();
+                //inList = context.vdp03.Local;
+                //Binding b = new Binding();
+                //b.Source = new string[] { "11", "22", "33", "11", "22", "33" };
+                //b.Source = new MyData();
+                //b.Source = pars;
 
-                             //parameterValues.SetBinding(ListBox.ItemsSourceProperty, b);
+                //parameterValues.SetBinding(ListBox.ItemsSourceProperty, b);
                 parameterValues.ItemsSource = pars;
 
 
@@ -92,9 +95,9 @@ namespace FurnacesInHand
         }
         private void MapTheRemoteBase()
         {
-            using (var context = new FurnacesModel()) //создали контекст взаимодействия с базой данных
+            using (var context = new FurnacesModelLocalNext()) //создали контекст взаимодействия с базой данных
             {
-                var pars = context.vdp03.ToArray<vdp03>();
+                var pars = context.vdp03.Where(x=>x.tagname=="Arc_U").OrderBy(x=>x.id).Take(9).ToArray<vdp03>();
                 conn = context.Database.Connection; //извлекли объект для соединения с БД
                 conn.Open(); //открыли соединение
                              MessageBox.Show(String.Format("PostgreSQL version is {0}",conn.ServerVersion));
@@ -118,18 +121,23 @@ namespace FurnacesInHand
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using (var contextToWhich = new FurnacesModelLocalNext()) //создали контекст взаимодействия с базой данных
+            using (var contextToWhich = new FurnacesModelLocal()) //создали контекст взаимодействия с базой данных
             {
-                using (var contextFromWhich = new FurnacesModelLocal()) //создали контекст взаимодействия с базой данных
+                contextToWhich.Database.Delete();
+                contextToWhich.Database.Create();
+                using (var contextFromWhich = new FurnacesModel()) //создали контекст взаимодействия с базой данных
                 {
-                    IQueryable<vdp03> inMemory = from x in contextFromWhich.vdp03 select x ;
+                    var inMemory = from x in contextFromWhich.vdp03 select x ;
                     int chunkSize = 100000;
-                    //contextToWhich.vdp03.RemoveRange(from x in contextToWhich.vdp03 select x);
-                    //contextToWhich.vdp03.AddRange(inMemory.Take<vdp03>(200000));
                     IQueryable<vdp03> restOfTheElements = inMemory;
-                    while(restOfTheElements.Count<vdp03>()>0)
-                    contextToWhich.vdp03.AddRange(restOfTheElements.Take<vdp03>(chunkSize);
-                    restOfTheElements = restOfTheElements.Skip<vdp03>(chunkSize);
+                    while (restOfTheElements.Count() > 0)
+                    {
+                        contextToWhich.vdp03.AddRange(restOfTheElements.Take(chunkSize));
+                        restOfTheElements = restOfTheElements.OrderBy(x=>x.id).Skip(chunkSize);
+                        
+
+                    }
+                    
                     contextToWhich.SaveChanges();
                     MessageBox.Show(String.Format("Number of entries {0}",inMemory.Count<vdp03>()));
 
