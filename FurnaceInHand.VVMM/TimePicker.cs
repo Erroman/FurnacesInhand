feda0a7d21c8e08b63384f04ae8e0f6ab1f25d37
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-04-01 14:32:21 +0500

    Added a project for ViewModel to bind TimePicker controls in the view

---
+++ FurnaceInHand.VVMM/TimePicker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceInHand.VVMM
+{
+    public class TimePicker
+    {
+        public DateTime  startTime;
+        public DateTime finishTime;
+    }
+}
