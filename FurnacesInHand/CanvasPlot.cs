4c072fc56c12e74fbade26a1139874d557928c9c
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-04-09 16:46:25 +0500

    Added CanvasPlot class fo drawing plots upon

---
+++ FurnacesInHand/CanvasPlot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FurnacesInHand
+{
+    class CanvasPlot:Canvas
+    {
+        private readonly VisualCollection _children;
+        // Provide a required override for the VisualChildrenCount property.
+        public CanvasPlot()
+        {
+            _children = new VisualCollection(this)
+            {
+                CreateDrawingVisualPlot()
+            };
+
+        }
+        private DrawingVisual CreateDrawingVisualPlot()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            Pen pen = new Pen(Brushes.Blue, 1.0);
+            return new DrawingVisual();
+        }
+        protected override int VisualChildrenCount => _children.Count;
+
+        // Provide a required override for the GetVisualChild method.
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= _children.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return _children[index];
+        }
+    }
+}
