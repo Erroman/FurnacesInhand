4c2dec1faf009bc5b3a492df67b708514435004d
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-04-12 16:28:47 +0500

    The first Voltage plot is built

--- FurnacesInHand/VoltageGraph.cs
+++ FurnacesInHand/VoltageGraph.cs
@@ -11,6 +11,7 @@ namespace FurnacesInHand
 {
     class VoltageGraph:FrameworkElement
     {
+        const int TicksInMillisecond = 10000; //
         private readonly DateTime startTime;
         private readonly DateTime finishTime;
         private readonly VisualCollection _children;
@@ -30,14 +31,14 @@ namespace FurnacesInHand
             DrawingVisual drawingVisual = new DrawingVisual();
             DrawingContext drawingContext = drawingVisual.RenderOpen();
             Pen pen = new Pen(Brushes.Black, 1.0);
-            double LowerLimitForTimeOnXAxis = 0;
-            double UpperLimitForTimeOnXAxis = 60000; //верхняя гравница
+            double LowerLimitForTimeOnXAxis = 0; //нижняя гравница временного интервала в миллисекундах
+            double UpperLimitForTimeOnXAxis = MillisecondsSinceTheBeginning(this.finishTime); //верхняя гравница временного интервала в миллисекундах
             double LowerLimitForVoltageOnYAxis = 0;
             double UpperLimitForVoltageOnYAxis = 50;
             double xmin = rect.X;
-            double xmax = rect.X+rect.Width;
+            double xmax = rect.X + rect.Width;
             double ymin = rect.Y;
-            double ymax = rect.Y+rect.Height;
+            double ymax = rect.Y + rect.Height;
             PrepareTransformations(
                 LowerLimitForTimeOnXAxis, UpperLimitForTimeOnXAxis,
                 LowerLimitForVoltageOnYAxis, UpperLimitForVoltageOnYAxis,
@@ -46,10 +47,35 @@ namespace FurnacesInHand
 
             Point begPoint = WtoD(new Point(20000, 10)); // миллисекунды, вольты: тестовая точка
             Point endPoint = WtoD(new Point(40000, 30)); // миллисекунды, вольты: тестовая точка
+            bool first_point = true;
+            foreach (var /*пара <время,значение параметра> */ time_parameter in timeParameterPairs)
+            {
+                DateTime? currrent_moment = time_parameter.dt; //время
+                double?   parameter_value = time_parameter.parameter; //значение праметра
+                
+                
+                if(currrent_moment!=null && parameter_value != null)
+                {
+                    begPoint = endPoint;
+                    endPoint = WtoD(new Point(MillisecondsSinceTheBeginning((DateTime)currrent_moment), (double)time_parameter.parameter));
+                    if (first_point)
+                    {
+                        first_point = false;
+                        begPoint = endPoint;
+                    }
+                    drawingContext.DrawLine(pen, begPoint, endPoint);
+                }
 
-            drawingContext.DrawLine(pen, begPoint, endPoint);
+                //begPoint = endPoint ?? time_parameter;
+            }
+            
             drawingContext.Close();
             return drawingVisual;
+        }
+        private double MillisecondsSinceTheBeginning(DateTime dt)
+        {
+
+            return (dt - this.startTime).Ticks / TicksInMillisecond;
         }
         protected override int VisualChildrenCount => _children.Count;
 
