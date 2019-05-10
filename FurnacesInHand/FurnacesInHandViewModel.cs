8e28bd9109a5bf8c684f29d2258222209c9d5308
Vladimir Romanovsky <Vladimir_Roman@mail.ru>
2019-05-10 22:24:14 +0500

    FurnacesInHandViewModel is put in the same main project and in the default namesace of this project

---
+++ FurnacesInHand/FurnacesInHandViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FurnacesInHand
+{
+    class FurnacesInHandViewModel
+    {
+        private double _canvasX;
+        private double _canvasY;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double CanvasX
+        {
+            get { return _canvasX; }
+            set
+            {
+                if (value.Equals(_canvasX)) return;
+                _canvasX = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double CanvasY
+        {
+            get { return _canvasY; }
+            set
+            {
+                if (value.Equals(_canvasY)) return;
+                _canvasY = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
