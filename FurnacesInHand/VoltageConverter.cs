67f762fc20c011af61eb3ebdbe27811ae0a9a27b
Vladimir Romanovsky <Vladimir_Roman@mail.ru>
2019-05-11 16:46:44 +0500

    Class VoltageConverter is added to conver X-coordinate on the Voltage canvas to the voltage value on the plot

---
+++ FurnacesInHand/VoltageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace FurnacesInHand
+{
+    class VoltageConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
