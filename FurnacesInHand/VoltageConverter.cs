695a4ece9397e458ccc07ae48d6d7f8b6080b45b
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-05-13 16:07:18 +0500

    Checking mouse left button state to decide what to output int the TextBox a a parameter's value in the VoltageConverter

--- FurnacesInHand/VoltageConverter.cs
+++ FurnacesInHand/VoltageConverter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Globalization;
+using System.Windows.Input;
 
 namespace FurnacesInHand
 {
@@ -12,7 +14,13 @@ namespace FurnacesInHand
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            ///Check the state of the left mouse button! And if not pressed, pass back the same value of the voltage,
+            ///else transform the X-coordinate coming in argument 'value'  to the voltage value corresponding to it
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                return value;
+            }
+            return "the old value is kept";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
