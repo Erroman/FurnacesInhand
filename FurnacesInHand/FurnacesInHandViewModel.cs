b1de876f2fb5fc3e367d32f313b957899bef1bed
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-06-18 13:12:15 +0500

    Properties are added to ViewModel to keep upper and lower limits for all parameters

--- FurnacesInHand/FurnacesInHandViewModel.cs
+++ FurnacesInHand/FurnacesInHandViewModel.cs
@@ -50,8 +50,100 @@ namespace FurnacesInHand
                 _VoltageMin = value;
                 OnPropertyChanged();
             }
-        }
 
+        }
+        private string _CurrentMax;
+        private string _CurrentMin;
+        public string CurrentMax
+        {
+            get { return _CurrentMax; }
+            set
+            {
+                if (value.Equals(_CurrentMax)) return;
+                _CurrentMax = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CurrentMin
+        {
+            get { return _CurrentMin; }
+            set
+            {
+                if (value.Equals(_CurrentMin)) return;
+                _CurrentMin = value;
+                OnPropertyChanged();
+            }
+
+        }
+        private string _VacuumMax;
+        private string _VacuumMin;
+        public string VacuumMax
+        {
+            get { return _VacuumMax; }
+            set
+            {
+                if (value.Equals(_VacuumMax)) return;
+                _VacuumMax = value;
+                OnPropertyChanged();
+            }
+        }
+        public string VacuumMin
+        {
+            get { return _VacuumMin; }
+            set
+            {
+                if (value.Equals(_VacuumMin)) return;
+                _VacuumMin = value;
+                OnPropertyChanged();
+            }
+
+        }
+        private string _SolenoidUMax;
+        private string _SolenoidUMin;
+        public string SolenoidUMax
+        {
+            get { return _SolenoidUMax; }
+            set
+            {
+                if (value.Equals(_SolenoidUMax)) return;
+                _SolenoidUMax = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SolenoidUMin
+        {
+            get { return _SolenoidUMin; }
+            set
+            {
+                if (value.Equals(_SolenoidUMin)) return;
+                _SolenoidUMin = value;
+                OnPropertyChanged();
+            }
+
+        }
+        private string _SolenoidIMax;
+        private string _SolenoidIMin;
+        public string SolenoidIMax
+        {
+            get { return _SolenoidIMax; }
+            set
+            {
+                if (value.Equals(_SolenoidIMax)) return;
+                _SolenoidIMax = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SolenoidIMin
+        {
+            get { return _SolenoidIMin; }
+            set
+            {
+                if (value.Equals(_SolenoidIMin)) return;
+                _SolenoidIMin = value;
+                OnPropertyChanged();
+            }
+
+        }
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
