cbb1dfbf466e3f6581bf15e03bb7738b6fec1bd2
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-06-27 08:03:24 +0500

    The code backing UserControl1 transferred into the code-back file from xaml.

--- TimeMover/UserControl1.xaml.cs
+++ TimeMover/UserControl1.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -21,5 +22,130 @@ partial class UserControl1 : UserControl
         InitializeComponent();
         clockWatch = (ClockWatch)Resources["newClock"];
     }
+
+        private void clockButton_Click(object O, RoutedEventArgs e)
+        {
+            Button someButton = O as Button;
+            switch (someButton.Name)
+            {
+                case "hoursUp":
+                    UpHour();
+                    break;
+                case "hoursDown":
+                    DownHour();
+                    break;
+                case "minutesUp":
+                    UpMinute();
+                    break;
+                case "minutesDown":
+                    DownMinute();
+                    break;
+                case "secondsUp":
+                    UpSecond();
+                    break;
+                case "secondsDown":
+                    DownSecond();
+                    break;
+                case "MillisecondsUp":
+                    UpTick();
+                    break;
+                case "MillisecondsDown":
+                    DownTick();
+                    break;
+            }
+        }
+        private void DownSecond()
+        {
+            if (clockWatch.Seconds > 0)
+                clockWatch.Seconds--;
+            else
+                if (DownMinute())
+                clockWatch.Seconds = 59;
+        }
+        private void UpSecond()
+        {
+            if (clockWatch.Seconds < 59)
+                clockWatch.Seconds++;
+            else
+                if (UpMinute())
+                clockWatch.Seconds = 0;
+        }
+
+        private bool DownMinute()
+        {
+            if (clockWatch.Minutes > 0)
+            {
+                clockWatch.Minutes--;
+                return true;
+            }
+            else
+
+                if (DownHour())
+            {
+                clockWatch.Minutes = 59;
+                return true;
+            }
+            else
+                return false;
+        }
+        private bool UpMinute()
+        {
+            if (clockWatch.Minutes < 59)
+            {
+                clockWatch.Minutes++;
+                return true;
+            }
+            else
+
+                if (UpHour())
+            {
+                clockWatch.Minutes = 0;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private bool DownHour()
+        {
+            if (clockWatch.Hours > 0)
+            {
+                clockWatch.Hours--;
+                return true;
+            }
+            else
+                return false;
+        }
+        private bool UpHour()
+        {
+            if (clockWatch.Hours < 23)
+            {
+                clockWatch.Hours++;
+                return true;
+            }
+            else
+                return false;
+        }
+        private bool UpTick()
+        {
+            if (clockWatch.Milliseconds < 1000)
+            {
+                clockWatch.Milliseconds++;
+                return true;
+            }
+            else
+                return false;
+        }
+        private bool DownTick()
+        {
+            if (clockWatch.Milliseconds > 0)
+            {
+                clockWatch.Milliseconds--;
+                return true;
+            }
+            else
+                return false;
+        }
     }
+
 }
