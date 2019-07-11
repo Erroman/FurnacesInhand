9dd4bac1a7518a99f959dafec91db3d8ccaf64dc
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-07-11 16:31:18 +0500

    New project VSMPO_AVISMAControls is added with brand new TimeMover being built

---
+++ FurnacesInHand/TimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace FurnacesInHand
+{
+    class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new DateTime(1001,11,1,7,40,40);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ VSMPO_AVISMAControls/clockwork.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSMPO_AVISMAControls
+{
+    class Clockwork : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DateTime _dt;
+        public DateTime Dt
+        {
+            get
+            {
+                return _dt;
+            }
+            set
+            {
+                _dt = value;
+                OnPropertyChanged(nameof(Dt));
+            }
+        }
+        private int _milliseconds;
+        public int Milliseconds
+        {
+            get
+            {
+                return _milliseconds;
+            }
+            set
+            {
+                _milliseconds = value;
+                OnPropertyChanged(nameof(Milliseconds));
+            }
+        }
+        private int _seconds;
+        public int Seconds
+        {
+            get
+            {
+                return _seconds;
+            }
+            set
+            {
+                _seconds = value;
+                OnPropertyChanged(nameof(Seconds));
+            }
+        }
+        private int _minutes;
+        public int Minutes
+        {
+            get
+            {
+                return _minutes;
+            }
+            set
+            {
+                _minutes = value;
+                OnPropertyChanged(nameof(Minutes));
+            }
+        }
+        private int _hours;
+        public int Hours
+        {
+            get
+            {
+                return _hours;
+            }
+            set
+            {
+                _hours = value;
+                OnPropertyChanged(nameof(Hours));
+            }
+        }
+        private DateTime _date;
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+        private void OnPropertyChanged(string property_name)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(property_name));
+        }
+    }
+}
