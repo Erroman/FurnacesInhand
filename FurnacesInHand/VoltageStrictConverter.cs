481f333cdfa81604b2387801e5cf91d1e0a5bb40
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-07-22 16:56:52 +0500

    Converter for choosing nearest DateTime value is prepared

---
+++ FurnacesInHand/VoltageStrictConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Input;
+using static FurnacesInHand.EnumerableExtensions;
+
+namespace FurnacesInHand
+{
+    class VoltageConverter:IValueConverter
+    {
+        private App _application;
+        private MainWindow _window;
+        public VoltageConverter()
+        {
+            _application = (App)Application.Current;
+            _window = (MainWindow)_application.MainWindow;
+        }
+
+        private double _lastMeasuredValue;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //value parameter holds a DateTime value obtained from TimeMover control
+            DateTime dt = (DateTime)value;
+            //Ближайшая по времени структура из считанного набора параметров
+            TimeParameterPair tpp;
+            if (_window.Voltage_graph_pairs != null)
+                {
+                //tpp = _window.Voltage_graph_pairs.Where(x => x.dt == _window.Voltage_graph_pairs.Max(x1 => x1.dt)).FirstOrDefault();
+                tpp = _window.Voltage_graph_pairs.OrderBy(x=>x.dt).FirstOrDefault();
+                _lastMeasuredValue = tpp.parameter;
+                }
+  
+            return _lastMeasuredValue; //presumably get it from the parameter argument
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
+//делаем так: при нажатой мышке ищем Х-координату её курсора в перечислении Voltage_graph_pairs<ParameterPair>, 
+//где в структуру ParameterPair добавлено поле Point screenPoint с занесёнными туда уже при рисовании графика значениями.
