5cfc5c8695228c28392da8773aa7b48658bf2b90
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-10-24 11:02:13 +0500

    Added a method to draw labels to the HorizontalRuler control

--- RulerControl/HorizontalRuler.xaml.cs
+++ RulerControl/HorizontalRuler.xaml.cs
@@ -113,7 +113,35 @@ namespace RulerControls
             
 
         }
+        // http://csharphelper.com/blog/2014/09/draw-a-graph-with-labels-wpf-c/ 
+        // Position a label at the indicated point.
+        private void DrawText(Canvas can, string text, Point location,
+            double font_size,
+            HorizontalAlignment halign, VerticalAlignment valign)
+        {
+            // Make the label.
+            Label label = new Label();
+            label.Content = text;
+            label.FontSize = font_size;
+            can.Children.Add(label);
 
+            // Position the label.
+            label.Measure(new Size(double.MaxValue, double.MaxValue));
+
+            double x = location.X;
+            if (halign == HorizontalAlignment.Center)
+                x -= label.DesiredSize.Width / 2;
+            else if (halign == HorizontalAlignment.Right)
+                x -= label.DesiredSize.Width;
+            Canvas.SetLeft(label, x);
+
+            double y = location.Y;
+            if (valign == VerticalAlignment.Center)
+                y -= label.DesiredSize.Height / 2;
+            else if (valign == VerticalAlignment.Bottom)
+                y -= label.DesiredSize.Height;
+            Canvas.SetTop(label, y);
+        }
         private void rulerBody_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             BuildTimeAxis();
