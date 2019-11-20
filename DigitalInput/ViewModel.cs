e91e75b8359c8bd6277ad000585ea5e730667c73
Vladimir Romanovsky <vladimir_roman@mail.ru>
2019-11-20 16:58:29 +0500

    A project is added to model digital input

---
+++ DigitalInput/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DigitalInput
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _digitalNumber;
+        public double DigitalNumber 
+        {
+            get { return _digitalNumber; }
+            set { _digitalNumber = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DigitalNumber))); }
+        }
+    }
+}
